c6842a0ae17c188b7fa54a6453847f4b7b697e23
Cuma KÖSE <turkmvc@gmail.com>
2024-03-27 22:21:22 +0300

    güncelleme Handler. 27.03.2024

--- src/Core/CleanArchitecture.Application/Features/Auth/Login/LoginCommandHandler.cs
+++ src/Core/CleanArchitecture.Application/Features/Auth/Login/LoginCommandHandler.cs
@@ -7,27 +7,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Application.Features.Auth.Login;
 
-//internal sealed class LoginCommandHandler(UserManager<AppUser> userManager, IJwtProvider jwtProvider, IMapper mapper) : IRequestHandler<LoginCommand, LoginCommandResponse>
-//{
-//    public async Task<LoginCommandResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
-//    {
-//        var user = await userManager.Users.Where(p => p.UserName == request.UserNameOrEmail || p.Email == request.UserNameOrEmail).FirstOrDefaultAsync(cancellationToken);
+internal sealed class LoginCommandHandler(UserManager<AppUser> userManager, IJwtProvider jwtProvider, IMapper mapper) : IRequestHandler<LoginCommand, LoginCommandResponse>
+{
+    public async Task<LoginCommandResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.Users.Where(p => p.UserName == request.UserNameOrEmail || p.Email == request.UserNameOrEmail).FirstOrDefaultAsync(cancellationToken);
 
-//        if (user is null)
-//        {
-//            throw new ArgumentException("Kullanıcı bulunamadı!");
-//        }
+        if (user is null)
+        {
+            throw new ArgumentException("Kullanıcı bulunamadı!");
+        }
 
-//        bool checkPassword = await userManager.CheckPasswordAsync(user, request.Password);
-//        if (!checkPassword)
-//        {
-//            throw new ArgumentException("Şifre yanlış!");
-//        }
+        bool checkPassword = await userManager.CheckPasswordAsync(user, request.Password);
+        if (!checkPassword)
+        {
+            throw new ArgumentException("Şifre yanlış!");
+        }
 
-//        var token = await jwtProvider.CreateTokenAsync(user, request.RememberMe);
+        var token = await jwtProvider.CreateTokenAsync(user, request.RememberMe);
 
 
-//        return new (StatusCode: 200, AccessToken: token.AccessToken, RefreshToken: user.RefreshToken, RehfreshTokenExpires: user.RefreshTokenExpires.Value, UserId: user.Id);
-//    }
-//}
+        return new(StatusCode: 200, AccessToken: token.AccessToken, RefreshToken: user.RefreshToken, RehfreshTokenExpires: user.RefreshTokenExpires.Value, UserId: user.Id);
+    }
+}
 
